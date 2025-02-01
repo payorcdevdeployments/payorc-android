4a23ebf56178bb7c4b40f74403c4c4465584b00d
amuthan <isaiamuthan.r@oclocksoftware.com>
2025-02-01 12:50:21 +0530

    Updated versions

--- payorcpayment/src/main/java/com/payorc/payment/config/PayOrcConfig.kt
+++ payorcpayment/src/main/java/com/payorc/payment/config/PayOrcConfig.kt
@@ -5,7 +5,7 @@ class PayOrcConfig {
     var merchantKey: String? = null
     var merchantSecret: String? = null
     var env: String? = null
-    var versionName: String = "1.0.1"
+    var versionName: String = "1.0.2"
 
     companion object : PayOrcSingleton<PayOrcConfig>(creator = { PayOrcConfig() })
 
